38c610383785f305ab3912c3edd3e46bb1a3f35f
RobbeBlaauw <730924@student.inholland.nl>
2025-09-02 10:47:25 +0200

    Update assignement1.cs

--- assignement1.cs
+++ assignement1.cs
@@ -1,6 +1,7 @@
 using system;
 
 namespace assignment1
+
 {
     class program
     {
@@ -11,7 +12,7 @@ namespace assignment1
             string age = Console.ReadLine();
             Console.WriteLine("your name is " + name);
             Console.WriteLine("your age is " + age);
-            Console.ReadKey();      
-             }
+            Console.ReadKey();
+        }
     }
 }
