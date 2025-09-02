04c9c94abb5b74b357e38ee014b3f4a4bc9a9a6f
RobbeBlaauw <730924@student.inholland.nl>
2025-09-02 09:52:45 +0200

    Create assignement1.cs

---
+++ assignement1.cs
@@ -0,0 +1,15 @@
+using system;
+
+namespace assignment1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            name = Console.Readline();
+            string age = Console.Readline();
+            Console.Writeline("your name is " + name);
+            Console.Writeline("your age is " + age);
+            Console.readkey();       }
+    }}
